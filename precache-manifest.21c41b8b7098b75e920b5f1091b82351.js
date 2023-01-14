726fd9f07e77ce67997407a82806c810e94a505b
Josuisme <josucortez@hotmail.com>
2023-01-13 18:11:27 -0600

    Updates

---
+++ precache-manifest.21c41b8b7098b75e920b5f1091b82351.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6919422e9afece0f17a8",
+    "url": "/amazon-web/static/css/main.465535c9.chunk.css"
+  },
+  {
+    "revision": "6919422e9afece0f17a8",
+    "url": "/amazon-web/static/js/main.6919422e.chunk.js"
+  },
+  {
+    "revision": "5fa7af8ecd3ec82e21da",
+    "url": "/amazon-web/static/js/1.5fa7af8e.chunk.js"
+  },
+  {
+    "revision": "e7d2a0e12eb3435394c2",
+    "url": "/amazon-web/static/js/runtime~main.e7d2a0e1.js"
+  },
+  {
+    "revision": "3cea38da8b6ad5654e8f2749841ee4e8",
+    "url": "/amazon-web/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/amazon-web/precache-manifest.21c41b8b7098b75e920b5f1091b82351.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/amazon-web/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
